efb5d923e95cbe7effb8c5d98c811ae12d9522b9
morganaribeiro <morganfrime2017@gmail.com>
2024-12-18 16:27:42 -0300

    refactor(*): create new avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilSimpleLine } from "@phosphor-icons/react";
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -9,7 +10,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/morganaribeiro.png" />
+        <Avatar src="https://github.com/morganaribeiro.png" />
         <strong>Morgana Ribeiro</strong>
         <span>Web Developer</span>
       </div>

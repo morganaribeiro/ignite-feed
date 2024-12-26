e051b599c835ec6b7c10ae86e46eb1b5ad0c6aca
Morgana Oliveira <morgana.ifce.2019@gmail.com>
2024-12-26 18:03:40 -0300

    feat(likes): add like counter in comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "@phosphor-icons/react";
 import styles from './Comment.module.css';
 import { Avatar } from "./Avatar";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
   function handleDeleteComment() {
     onDeleteComment(content);
+  };
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   };
 
   return (
@@ -29,9 +35,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span> 
+            Aplaudir <span>{likeCount}</span> 
           </button>
         </footer>
       </div>
